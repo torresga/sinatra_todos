589921b52d01e6004189700a2036759e6f69baf9
Gelsey Torres <gelsey.torres@gmail.com>
2020-01-14 11:34:30 -0500

    Adds code to close db connection

--- database_persistence.rb
+++ database_persistence.rb
@@ -2,13 +2,22 @@ require "pg"
 
 class DatabasePersistence
   def initialize(logger)
-    @db = PG.connect(dbname: "todos")
+    @db = if Sinatra::Base.production?
+      PG.connect(ENV['DATABASE_URL'])
+    else
+      PG.connect(dbname: "todos")
+    end
+
     @logger = logger
   end
 
   def query(statement, *params)
     @logger.info "#{statement}: #{params}"
     @db.exec_params(statement, params)
+  end
+
+  def disconnect
+    @db.close
   end
 
   def find_list(id)
--- todo.rb
+++ todo.rb
@@ -74,6 +74,10 @@ before do
   @storage = DatabasePersistence.new(logger)
 end
 
+after do
+  @storage.disconnect
+end
+
 get '/' do
   redirect '/lists'
 end
